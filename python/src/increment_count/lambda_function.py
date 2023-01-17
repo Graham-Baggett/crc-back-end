1b146277c9fd097d01c70fd7e99c00a8b9bcd527
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-01-17 11:39:23 -0500

    Replaced .gitkeep with lambda_function.py in return_count and increment_count folders

---
+++ python/src/increment_count/lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('home_page')
+
+def lambda_handler(event, context):
+    response = table.get_item(Key={
+            'record_id':'1'
+    })
+    visitor_count = response['Item']['visitor_count']
+    visitor_count = visitor_count + 1
+    print(visitor_count)
+    response = table.put_item(Item={
+            'record_id':'1',
+            'visitor_count': visitor_count
+    })
+    
+    return "Visitor count incremented successfully!"
