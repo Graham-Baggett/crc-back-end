d8987db1fb4d1ff676464dc5ad10956a3fd245b6
Graham L Baggett <graham.l.baggett@census.gov>
2023-01-26 13:57:23 -0500

    added region to boto3 dynamodb resource

--- python/src/return_count/lambda_get_visitor_count.py
+++ python/src/return_count/lambda_get_visitor_count.py
@@ -1,12 +1,11 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('home_page')
+dynamodb = boto3.resource("dynamodb", "us-east-1")
+table = dynamodb.Table("home_page")
+
 
 def lambda_handler(event, context):
-    response = table.get_item(Key={
-       'record_id':'1'
-    })
-    
-    return response['Item']['visitor_count']
+    response = table.get_item(Key={"record_id": "1"})
+
+    return response["Item"]["visitor_count"]
