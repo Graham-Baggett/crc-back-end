5777d3cd5f8381bad6443c150e6056f065d534bc
Graham L Baggett <graham.l.baggett@census.gov>
2023-02-02 15:53:11 -0500

    isolated functionality in lambda functions
    I created a separate function in each lambda function py file to be able to test the functions with a mock dynamodb

--- python/src/increment_count/lambda_increment_visitor_count.py
+++ python/src/increment_count/lambda_increment_visitor_count.py
@@ -1,15 +1,19 @@
-import json
 import boto3
 
-dynamodb = boto3.resource("dynamodb", "us_east_1")
-table = dynamodb.Table("home_page")
 
-
-def lambda_handler(event, context):
-    response = table.get_item(Key={"record_id": "1"})
+def increment_count(ddb_table):
+    response = ddb_table.get_item(Key={"record_id": "1"})
     visitor_count = response["Item"]["visitor_count"]
     visitor_count = visitor_count + 1
     print(visitor_count)
-    response = table.put_item(Item={"record_id": "1", "visitor_count": visitor_count})
+    response = ddb_table.put_item(
+        Item={"record_id": "1", "visitor_count": visitor_count}
+    )
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource("dynamodb", "us_east_1")
+    table = dynamodb.Table("home_page")
+    increment_count(table)
 
     return "Visitor count incremented successfully!"
--- python/src/return_count/lambda_get_visitor_count.py
+++ python/src/return_count/lambda_get_visitor_count.py
@@ -1,11 +1,13 @@
-import json
 import boto3
 
-dynamodb = boto3.resource("dynamodb", "us-east-1")
-table = dynamodb.Table("home_page")
+
+def get_count(ddb_table):
+    response = ddb_table.get_item(Key={"record_id": "1"})
+    return response["Item"]["visitor_count"]
 
 
 def lambda_handler(event, context):
-    response = table.get_item(Key={"record_id": "1"})
+    dynamodb = boto3.resource("dynamodb", "us-east-1")
+    table = dynamodb.Table("home_page")
 
-    return response["Item"]["visitor_count"]
+    return get_count(table)
