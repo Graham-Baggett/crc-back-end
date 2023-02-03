e43e3dc4dbd2ad554e364a63b939567f3dc1f21d
Graham L Baggett <graham.l.baggett@census.gov>
2023-02-03 09:53:49 -0500

    test file now uses pytest fixtures
    The test file uses fixtures (and the 'with...yield' pattern), which should result in connection closing when the test is done

--- python/tests/test_lambda_function.py
+++ python/tests/test_lambda_function.py
@@ -1,42 +1,42 @@
 from python.src.return_count import lambda_get_visitor_count
 from python.src.increment_count import lambda_increment_visitor_count
 import boto3
+import pytest
 from moto import mock_dynamodb
 
 
-@mock_dynamodb
-def create_table(dynamodb_connection, table_name):
-
-    return dynamodb_connection.create_table(
-        TableName=table_name,
-        KeySchema=[{"AttributeName": "record_id", "KeyType": "HASH"}],
-        AttributeDefinitions=[
-            {"AttributeName": "record_id", "AttributeType": "S"},
-        ],
-        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
-    )
+table_name = "home_page"
 
 
-@mock_dynamodb
-def get_dynamodb_connection():
-    """DynamoDB mock client."""
-    conn = boto3.resource("dynamodb", region_name="us-east-1")
+@pytest.fixture
+def ddb_connection():
+    with mock_dynamodb():
+        ddb_conn = boto3.resource("dynamodb", "us-east-1")
+        yield ddb_conn
 
-    return conn
+
+@pytest.fixture
+def ddb_table(ddb_connection):
+
+    with mock_dynamodb():
+        yield ddb_connection.create_table(
+            TableName=table_name,
+            KeySchema=[{"AttributeName": "record_id", "KeyType": "HASH"}],
+            AttributeDefinitions=[
+                {"AttributeName": "record_id", "AttributeType": "S"},
+            ],
+            ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+        )
 
 
-@mock_dynamodb
-def test_lambda_handler():
+def test_lambda_functions(ddb_table):
 
-    table_name = "home_page"
-    ddb_conn = get_dynamodb_connection()
-    table = create_table(ddb_conn, table_name)
     data = {"record_id": "1", "visitor_count": 20}
-    table.put_item(TableName=table_name, Item=data)
-    response = table.get_item(Key={"record_id": "1"})
+    ddb_table.put_item(TableName=table_name, Item=data)
+    response = ddb_table.get_item(Key={"record_id": "1"})
     visitor_count = response["Item"]["visitor_count"]
-    actual_output = lambda_get_visitor_count.get_count(table)
+    actual_output = lambda_get_visitor_count.get_count(ddb_table)
     assert actual_output == visitor_count
-    lambda_increment_visitor_count.increment_count(table)
-    incremented_count = lambda_get_visitor_count.get_count(table)
+    lambda_increment_visitor_count.increment_count(ddb_table)
+    incremented_count = lambda_get_visitor_count.get_count(ddb_table)
     assert incremented_count == visitor_count + 1
