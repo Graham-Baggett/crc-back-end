18f315f38ff45f3de7a11afc0a51606443a2bc7b
Graham Baggett <graham.l.baggett@census.gov>
2023-03-07 11:41:49 -0500

    Updated lambda get count based on ChatGPT feedback
    I added the following things to lambda_get_visitor_count (based on ChatGPT):
    
    1. Type hints for readability
    2. Error handling
    3. Removed unecessary "Access-Control-Allow-Credentials' header
    4. Used constants instead of hardcoded values

--- python/src/return_count/lambda_get_visitor_count.py
+++ python/src/return_count/lambda_get_visitor_count.py
@@ -1,22 +1,43 @@
 import boto3
+from typing import Any
+
+DYNAMODB_REGION = "us-east-1"
+DYNAMODB_TABLE = "home_page"
+PRIMARY_KEY = "record_id"
+PRIMARY_KEY_VALUE = "1"
+VISITOR_COUNT_KEY = "visitor_count"
 
 
-def get_count(ddb_table):
-    response = ddb_table.get_item(Key={"record_id": "1"})
-    return response["Item"]["visitor_count"]
+def get_count(ddb_table: Any) -> int:
+    try:
+        response = ddb_table.get_item(Key={PRIMARY_KEY: PRIMARY_KEY_VALUE})
+        return response["Item"][VISITOR_COUNT_KEY]
+    except Exception as e:
+        print(f"Error fetching visitor count: {e}")
+        return -1
 
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource("dynamodb", "us-east-1")
-    table = dynamodb.Table("home_page")
+def lambda_handler(event: dict, context: Any) -> dict:
+    dynamodb = boto3.resource("dynamodb", DYNAMODB_REGION)
+    table = dynamodb.Table(DYNAMODB_TABLE)
+
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Content-Type": "application/json",
+    }
+
+    count = get_count(table)
+
+    if count == -1:
+        return {
+            "statusCode": 500,
+            "headers": headers,
+            "body": {"error": "Failed to fetch visitor count"},
+        }
 
     return {
         "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Credentials": "*",
-            "Content-Type": "application/json",
-        },
-        "body": get_count(table),
+        "headers": headers,
+        "body": {"visitor_count": count},
     }
