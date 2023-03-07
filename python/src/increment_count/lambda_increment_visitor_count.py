6c537e54e8e12319234bc88c54aa14057a2a0f5f
Graham Baggett <graham.l.baggett@census.gov>
2023-03-07 12:31:15 -0500

    Streamlined the code
    I streamlined the lambda incrementer by reducing duplication.
    Per ChatGPT, I also added variables to replace hardcoding.
    Now the increment function looks like the return count function

--- python/src/increment_count/lambda_increment_visitor_count.py
+++ python/src/increment_count/lambda_increment_visitor_count.py
@@ -1,14 +1,20 @@
 import boto3
 
+DYNAMODB_REGION = "us-east-1"
+DYNAMODB_TABLE = "home_page"
+PRIMARY_KEY = "record_id"
+PRIMARY_KEY_VALUE = "1"
+VISITOR_COUNT_KEY = "visitor_count"
+
 
 def increment_count(ddb_table):
     try:
         # Get the current visitor count from DynamoDB
-        response = ddb_table.get_item(Key={"record_id": "1"})
+        response = ddb_table.get_item(Key={PRIMARY_KEY: PRIMARY_KEY_VALUE})
 
         # If the item exists, increment the visitor count by 1
         if "Item" in response:
-            visitor_count = response["Item"]["visitor_count"]
+            visitor_count = response["Item"][VISITOR_COUNT_KEY]
             visitor_count = visitor_count + 1
             print(visitor_count)
         # If the item doesn't exist, set the visitor count to 1
@@ -17,14 +23,13 @@ def increment_count(ddb_table):
 
         # Update the visitor count in DynamoDB
         response = ddb_table.put_item(
-            Item={"record_id": "1", "visitor_count": visitor_count}
+            Item={PRIMARY_KEY: PRIMARY_KEY_VALUE, VISITOR_COUNT_KEY: visitor_count}
         )
         return {
             "statusCode": 200,
             "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Credentials": "*",
                 "Content-Type": "application/json",
             },
             "body": f"Visitor count incremented successfully!",
@@ -37,7 +42,6 @@ def increment_count(ddb_table):
             "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Credentials": "*",
                 "Content-Type": "application/json",
             },
             "body": f"Error: {str(e)}",
@@ -46,35 +50,10 @@ def increment_count(ddb_table):
 
 def lambda_handler(event, context):
     # Create a DynamoDB resource
-    dynamodb = boto3.resource("dynamodb", "us-east-1")
+    dynamodb = boto3.resource("dynamodb", DYNAMODB_REGION)
 
     # Get a reference to the home_page table
-    table = dynamodb.Table("home_page")
+    table = dynamodb.Table(DYNAMODB_TABLE)
 
     # Increment the visitor count for the home page
-    increment_count(table)
-
-    # Return a success message
-    try:
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Credentials": "*",
-                "Content-Type": "application/json",
-            },
-            "body": f"Visitor count incremented successfully!",
-        }
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return {
-            "statusCode": 500,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Credentials": "*",
-                "Content-Type": "application/json",
-            },
-            "body": f"Error: {str(e)}",
-        }
+    return increment_count(table)
