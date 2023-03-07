9532dfe01271057525bd3669dbe07b942eff75a1
Graham Baggett <graham.l.baggett@census.gov>
2023-03-07 12:23:54 -0500

    Updated python tests based on ChatGPT feedback
    Implemented the following ChatGPT recommendations:
    
    1. Removed unnecessary mock_dynamodb() calls
    2. Used variables to replace hardcoding
    3. Added a fail message to the assert for the scenario where the test fails

--- python/tests/test_lambda_function.py
+++ python/tests/test_lambda_function.py
@@ -5,37 +5,40 @@ import pytest
 from moto import mock_dynamodb
 
 
-table_name = "home_page"
+DYNAMODB_REGION = "us-east-1"
+DYNAMODB_TABLE = "home_page"
+PRIMARY_KEY = "record_id"
+PRIMARY_KEY_VALUE = "1"
+VISITOR_COUNT_KEY = "visitor_count"
 
 
 @pytest.fixture
 def ddb_connection():
     with mock_dynamodb():
-        ddb_conn = boto3.resource("dynamodb", "us-east-1")
-        yield ddb_conn
+        yield boto3.resource("dynamodb", DYNAMODB_REGION)
 
 
 @pytest.fixture
 def ddb_table(ddb_connection):
-
-    with mock_dynamodb():
-        yield ddb_connection.create_table(
-            TableName=table_name,
-            KeySchema=[{"AttributeName": "record_id", "KeyType": "HASH"}],
-            AttributeDefinitions=[
-                {"AttributeName": "record_id", "AttributeType": "S"},
-            ],
-            ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
-        )
+    return ddb_connection.create_table(
+        TableName=DYNAMODB_TABLE,
+        KeySchema=[{"AttributeName": PRIMARY_KEY, "KeyType": "HASH"}],
+        AttributeDefinitions=[
+            {"AttributeName": PRIMARY_KEY, "AttributeType": "S"},
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+    )
 
 
 def test_lambda_functions(ddb_table):
 
     # Increment runs first because it can populate the table if empty
     lambda_increment_visitor_count.increment_count(ddb_table)
-    response = ddb_table.get_item(Key={"record_id": "1"})
-    visitor_count = response["Item"]["visitor_count"]
+    response = ddb_table.get_item(Key={PRIMARY_KEY: PRIMARY_KEY_VALUE})
+    visitor_count = response["Item"][VISITOR_COUNT_KEY]
     # Increment a second time to create a comparison between the original and the new visitor_count value
     lambda_increment_visitor_count.increment_count(ddb_table)
     incremented_count = lambda_get_visitor_count.get_count(ddb_table)
-    assert incremented_count == visitor_count + 1
+    assert (
+        incremented_count == visitor_count + 1
+    ), "increment_count did not increment the visitor count correctly."
