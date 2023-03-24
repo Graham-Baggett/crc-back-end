fc2f020a3fd8d6a0cdd3e03e1d5d836d5c8de3bd
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 10:02:16 -0400

    Add API Gateway Lambda permission to lambda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -111,3 +111,14 @@ resource "aws_iam_role" "increment_visitor_count_function_role" {
     })
   }
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.example.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
