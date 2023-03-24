f3b634c3c8d8c28e1997a757f40e13f86df8ba52
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 10:48:29 -0400

    Update api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -3,10 +3,16 @@ resource "aws_api_gateway_rest_api" "crc_api_infra_api" {
   description = "API Gateway for the Visitor Count Lambda functions"
 }
 
-resource "aws_api_gateway_resource" "prod" {
+resource "aws_api_gateway_resource" "get" {
   rest_api_id = aws_api_gateway_rest_api.crc_api_infra_api.id
   parent_id   = aws_api_gateway_rest_api.crc_api_infra_api.root_resource_id
-  path_part   = "Prod"
+  path_part   = "get"
+}
+
+resource "aws_api_gateway_resource" "put" {
+  rest_api_id = aws_api_gateway_rest_api.crc_api_infra_api.id
+  parent_id   = aws_api_gateway_rest_api.crc_api_infra_api.root_resource_id
+  path_part   = "put"
 }
 
 resource "aws_api_gateway_method" "get" {
@@ -16,9 +22,9 @@ resource "aws_api_gateway_method" "get" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "get" {
+resource "aws_api_gateway_integration" "apigw-integration-get" {
   rest_api_id = aws_api_gateway_rest_api.crc_api_infra_api.id
-  resource_id = aws_api_gateway_resource.crc_api_infra_api.id
+  resource_id = aws_api_gateway_resource.get.id
   http_method = aws_api_gateway_method.get.http_method
 
   integration_http_method = "GET"
@@ -26,10 +32,20 @@ resource "aws_api_gateway_integration" "get" {
   uri                     = aws_lambda_function.get_visitor_count_function.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_integration" "apigw-integration-put" {
+  rest_api_id = aws_api_gateway_rest_api.crc_api_infra_api.id
+  resource_id = aws_api_gateway_resource.put.id
+  http_method = aws_api_gateway_method.get.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.increment_visitor_count_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "apigw-deployment" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.apigw-integration-get,
+    aws_api_gateway_integration.apigw-integration-put
   ]
 
   rest_api_id = aws_api_gateway_rest_api.crc_api_infra_api.id
