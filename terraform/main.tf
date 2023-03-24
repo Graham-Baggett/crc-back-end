64be8417bc3efe724e439aa0cf286417ac1a4d61
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 09:22:56 -0400

    Removed DDB config from main.tf
    Going to move it to dynamodb.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "visitor_count_table" {
-  name           = var.table_name
-  billing_mode   = var.billing_mode
-  hash_key       = var.attribute_name
-  attribute {
-    name = var.attribute_name
-    type = "S"
-  }
-}
-
 resource "aws_lambda_function" "get_visitor_count_function" {
   function_name = "get-visitor-count-function"
   role          = aws_iam_role.get_visitor_count_function_role.arn
