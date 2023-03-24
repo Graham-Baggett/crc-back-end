4ce6066d466477ad328a9c63991d00adc5d3f33c
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 11:18:51 -0400

    Added account_id to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 variable "api_url" {
   type        = string
   default     = "https://r31nk3e4ck.execute-api.${var.region}.amazonaws.com"
@@ -8,4 +10,10 @@ variable "region" {
   type        = string
   default     = "us-east-1"
   description = "Default region for the application"
+}
+
+variable "account_id" {
+  type        = string
+  default     = data.aws_caller_identity.current.account_id
+  description = "AWS Account ID"
 }
