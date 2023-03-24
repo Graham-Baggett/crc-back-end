c1038675ee4463862781e91182ae456acaf3b7ff
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 13:12:17 -0400

    Formatted dynamodb.tf to conform with terraform fmt

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "visitor_count_table" {
-  name           = var.visitor_count_table_name
-  billing_mode   = var.billing_mode
-  hash_key       = var.attribute_name
+  name         = var.visitor_count_table_name
+  billing_mode = var.billing_mode
+  hash_key     = var.attribute_name
   attribute {
     name = var.attribute_name
     type = "S"
