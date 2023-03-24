1daf55c9d5c9e2b7e8bda3c90efbdc267f7f2ec9
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 12:08:55 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,21 @@ variable "put_path_part" {
   default     = "put"
   description = "Name of the path ending for the Put Visitor Count Function"
 }
+
+variable "visitor_count_table_name" {
+  type        = string
+  default     = "home_page"
+  description = "Name of the DynamoDB table that stores the visitor count"
+}
+
+variable "billing_mode" {
+  type        = string
+  default     = "PAY_PER_REQUEST"
+  description = "Billing mode of the DynamoDB table that stores the visitor count"
+}
+
+variable "attribute_name" {
+  type        = string
+  default     = "record_id"
+  description = "Primary key name of the DynamoDB table that stores the visitor count"
+}
