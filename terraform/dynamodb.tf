2dfe71db4e816008e6bfbdda8b06f3d7ea561bf6
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-24 12:09:56 -0400

    Updated name of DynamoDB table name variable

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "visitor_count_table" {
-  name           = var.table_name
+  name           = var.visitor_count_table_name
   billing_mode   = var.billing_mode
   hash_key       = var.attribute_name
   attribute {
