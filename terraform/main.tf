c6d4dadc1111492fa1eb6e3659321d1f64ed4ec3
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-27 09:57:31 -0400

    Added state management to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "gb-crc-terraform-state"
+    key            = "back-end/terraform.tfstate"
+    region         = var.region
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
 }
