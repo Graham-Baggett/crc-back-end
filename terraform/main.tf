fe9d812cbd40daab3fdc5319a9c1462d3b35c061
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-03-27 10:00:08 -0400

    Changed var.region back to us-east-1 in main.tf backend block

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "gb-crc-terraform-state"
     key            = "back-end/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
