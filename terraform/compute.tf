4def4eb76a55dbbffab92e15b106031a9baf9de2
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-06-15 14:55:48 -0400

    Create compute.tf file for OCI VM instance [skip ci]

---
+++ terraform/compute.tf
@@ -0,0 +1,51 @@
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
+
+data "oci_core_images" "oracle_linux_images" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Oracle Linux"
+  operating_system_version = "8"
+  shape                    = var.instance_shape
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
+
+resource "oci_core_instance" "free_instance1" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "freeInstance1"
+  shape               = var.instance_shape
+
+  shape_config {
+    ocpus = 1
+    memory_in_gbs = 6
+  }
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.public_subnet.id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "freeinstance1"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = lookup(data.oci_core_images.oracle_linux_images.images[0], "id")
+  }
+
+  metadata = {
+    ssh_authorized_keys = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.public_key_openssh
+  }
+}
+
+resource "tls_private_key" "compute_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+output "generated_private_key_pem" {
+  value     = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.private_key_pem
+  sensitive = true
+}
