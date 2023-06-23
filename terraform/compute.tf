bcf4b8779c4b125a531950376a60bdb16743ece1
Graham Baggett <122628449+Graham-Baggett@users.noreply.github.com>
2023-06-23 11:35:31 -0400

    Attempt to create Oracle VM Instance

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -20,39 +20,39 @@ data "oci_identity_availability_domain" "ad" {
   ad_number      = 1
 }
 
-#resource "oci_core_instance" "free_instance2" {
-#  availability_domain = data.oci_identity_availability_domain.ad.name
-#  compartment_id      = var.compartment_ocid
-#  display_name        = "freeInstance2"
-#  shape               = "VM.Standard.E2.1.Micro"
+resource "oci_core_instance" "free_instance2" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = "freeInstance2"
+  shape               = "VM.Standard.E2.1.Micro"
 
-#  shape_config {
-#    ocpus = 1
-#    memory_in_gbs = 1
-#  }
+  shape_config {
+    ocpus = 1
+    memory_in_gbs = 1
+  }
 
-#  create_vnic_details {
-#    subnet_id        = data.oci_core_subnets.public_subnets.subnets[0].id
-#    display_name     = "primaryvnic"
-#    assign_public_ip = true
-#    hostname_label   = "freeinstance2"
-#  }
+  create_vnic_details {
+    subnet_id        = data.oci_core_subnets.public_subnets.subnets[0].id
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "freeinstance2"
+  }
 
-#  source_details {
-#    source_type = "image"
-#    source_id   = lookup(data.oci_core_images.oracle_linux_images.images[0], "id")
-#    boot_volume_size_in_gbs = 100
-#  }
+  source_details {
+    source_type = "image"
+    source_id   = lookup(data.oci_core_images.oracle_linux_images.images[0], "id")
+    boot_volume_size_in_gbs = 100
+  }
 
-#  metadata = {
-#    ssh_authorized_keys = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.public_key_openssh
-#  }
-#}
+  metadata = {
+    ssh_authorized_keys = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.public_key_openssh
+  }
+}
 
-#resource "tls_private_key" "compute_ssh_key" {
-#  algorithm = "RSA"
-#  rsa_bits  = 2048
-#}
+resource "tls_private_key" "compute_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
 
 #output "generated_private_key_pem" {
 #  value     = (var.ssh_public_key != "") ? var.ssh_public_key : tls_private_key.compute_ssh_key.private_key_pem
