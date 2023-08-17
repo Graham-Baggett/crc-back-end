62dd6fa3e21c2129d580bf2227a5f7529a371786
Graham Baggett <graham.l.baggett@census.gov>
2023-08-17 11:51:20 -0400

    Using an SSLConect object to manage certs

--- python/src/visitor_counter.py
+++ python/src/visitor_counter.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, request
 from flask_cors import CORS
+import json
 import os
 import oracledb
-import json
+import ssl
+
 
 app = Flask(__name__)
 CORS(app)
@@ -53,4 +55,16 @@ def json_response(obj, status=200):
 if __name__ == "__main__":
     cert_file = os.environ.get("SSL_CERT_FILE")
     key_file = os.environ.get("SSL_KEY_FILE")
-    app.run(host="0.0.0.0", port=8080, ssl_context=(cert_file, key_file))
+
+    # Read the certificate and key file contents
+    with open(cert_file, "r") as cert_file:
+        cert_data = cert_file.read()
+    with open(key_file, "r") as key_file:
+        key_data = key_file.read()
+
+    # Create an SSL context with the loaded certificate and key data
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile=cert_data, keyfile=key_data)
+
+    # Run the app with the SSL context
+    app.run(host="0.0.0.0", port=8080, ssl_context=ssl_context)
